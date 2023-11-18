cd33131c711bad350c603d76186ea598bdab57a0
RanelDL <ranel30d@gmail.com>
2023-11-18 02:10:38 +0200

    added chrome user data path prompt

--- src/loadLocations.py
+++ src/loadLocations.py
@@ -7,12 +7,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+import tkinter as tk
+from tkinter import filedialog
+
 class Automaps:
 
     def __init__(self, url):
         self.url = url
+        self.USER_DATA_PATH = self.set_chrome_user_path()
         self.driver = self.launch_page()
 
+    def set_chrome_user_path(self):
+        # open txt file
+        path = ""
+        try:
+            with open("ChromeUserPath.txt", 'a+') as file:
+                pos = file.tell()
+                # if txt file not empty: read file contents as path
+                if  pos != 0:
+                    file.seek(0,0) # opened on append, cursor has to be reset
+                    path = file.readline() 
+
+                # empty, open file explorer 
+                else: 
+                    root = tk.Tk()
+                    root.withdraw()
+                    path = filedialog.askdirectory()
+                    file.write(path)
+        except Exception as e:
+            print( "error opening path file: ", e)    
+
+        return path
+        
     def clear_searchbar(self):
         clear_button = self.driver.find_element(By.CSS_SELECTOR, "#searchbox > div.lSDxNd > button")
         clear_button.click()
@@ -20,8 +46,9 @@ class Automaps:
     def launch_page(self):
         # Chrome options to load signed-in chrome profile
         chrome_options = Options()
-        chrome_options.add_argument("--user-data-dir=C:\\Users\\USER\\AppData\\Local\\Google\\Chrome for Testing\\User Data\\") # Opens chrome profile
-        #chrome_options.add_argument("--profile-directory=Default")
+        # Path should be something like:
+        # "C:\\Users\\USER\\AppData\\Local\\Google\\Chrome for Testing\\User Data\\") # Opens chrome profile
+        chrome_options.add_argument("--user-data-dir=" + self.USER_DATA_PATH) 
         chrome_options.add_experimental_option("detach", True)
 
         driver  = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
@@ -35,19 +62,18 @@ class Automaps:
     def enter_address(self, address):
         # Searchbar element
         textbox = self.driver.find_element(By.ID, "searchboxinput") # or XmI62e for the form itself
-        # Input address into searchbox , add petah tikva prefix to each local address
-        textbox.send_keys("פתח תקווה," + address )#text)#address + " פתח תקווה ")
+        textbox.send_keys( address )#text)#address + " פתח תקווה ")
         textbox.send_keys(Keys.ENTER)
 
     def save_to_list(self, listName): 
         button = self.driver.find_element(By.CSS_SELECTOR, "#QA0Szd>div>div>div.w6VYqd>div.bJzME.tTVLSc>div>div.e07Vkf.kA9KIf>div>div>div.m6QErb.Pf6ghf.ecceSd.tLjsW>div:nth-child(2)>button")
-        foo = "Bouldering"
         button.click()
         # bombSheltersList = self.driver.find_element(By.CSS_SELECTOR, "#fDahXd div:nth-of-type(6)") # this just saves to the 6th indexed list in the dropdown menu
         # bombSheltersList = self.driver.find_element(By.XPATH,"//div[text()='" + foo + "']") # why does this xpath leads to error: Message:elemnt click intercepted:... 
         # ... Other element would receive the click: ...
-        bombSheltersList = self.driver.find_element(By.XPATH,"//div[.='" + foo + "']")
+        bombSheltersList = self.driver.find_element(By.XPATH,"//div[.='" + listName + "']")
         bombSheltersList.click()
+
     def add_note(self, note):
         button = self.driver.find_element(By.CSS_SELECTOR, "#QA0Szd > div > div > div.w6VYqd > div.bJzME.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div:nth-child(7) > div > div.m6QErb.Duzcee > div.m6QErb.tLjsW > div:nth-child(1) > button > div")
         button.click()
@@ -59,7 +85,7 @@ class Automaps:
 def main():
     GMAPS_URL ="https://www.google.com/maps/@32.0851663,34.8955189,15z?authuser=0&entry=ttu" 
     auto = Automaps(GMAPS_URL)
-    LIST_NAME = ""
+    LIST_NAME = "Bouldering"
     '''
     #driver = launch_gmaps()
     # with open("addresses_wrk_cpy.txt", 'rt') as ADDR_FILE:
@@ -87,7 +113,8 @@ def main():
     auto.enter_address("orlife")
     time.sleep(2)
     try:
-        auto.save_to_list()
+        # auto.save_to_list()
+        pass
     except Exception as e:
         print(e)
     time.sleep(4)
